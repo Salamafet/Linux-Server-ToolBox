7a4a7bf3bfc9b6280d9e03b86b6903b37de6748c
Salamafet <salamafet@msn.com>
2017-06-06 09:31:50 +0200

    merge function_temp file with header + delete

--- header.js
+++ header.js
@@ -29,5 +29,33 @@ mini_text = `
 
 function firewall()
 {
+	var items = [
+	'List Open Port' ,
+	'Open Port' ,
+	'Close Port',
+	'Back to Main Menu'
+	] ;
+
+	term.gridMenu( items , function( error , response ) {
+	term( '\n' ).eraseLineAfter.green(
+		"#%s selected: %s (%s,%s)\n" ,
+		response.selectedIndex ,
+		response.selectedText ,
+		response.x ,
+		response.y
+	);
+	switch(response.selectedIndex) {
+		case 0:
+			term.brightBlue("Open port list:");
+			break;
+		case 1:
+			term.brightBlue("Enter the port to open: ");
+			break;
+		case 2:
+			term.brightBlue("Enter the port to close: ");
+		default:
+			term.brightYellow("Exit to main menu");
+}
+	} ) ;
 
 }
